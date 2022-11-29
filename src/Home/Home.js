b981e72fd3491156b718f773f0dcbde0c8c9b0f6
stivflur-vlz <stiven.vlz2191@gmail.com>
2022-11-29 16:39:00 -0500

    se crea proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,93 @@
+import "./Home.css";
+export function Home() {
+  return (
+    <>
+      <section>
+        <div class="banner"></div>
+      </section>
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-8">
+              <h2>RBD</h2>
+              <p>
+              RBD es un grupo de pop mexicano, formado por los cantantes y actores Anahí, Dulce María, Maite Perroni, Alfonso Herrera, Christopher Uckermann y Christian Chávez, originario de la Ciudad de México. Fue creado por Pedro Damián, dentro de la telenovela Rebelde. Se formó el 4 de octubre de 2004 hasta su separación, y su último concierto fue en Madrid, España el 21 de diciembre de 2008. En 2020, la banda regresó a la música con Anahí, Maite Perroni, Christopher von Uckermann y Christian Chávez, cuatro de sus integrantes originales. Alfonso Herrera y Dulce María no regresaron en su reencuentro.
+              <br/>
+              En noviembre de 2004, lanzan su álbum debut titulado Rebelde. En septiembre de 2005, lanzan su segundo álbum de estudio, Nuestro amor, recibiendo su primera nominación en los Grammy Latinos. En 2006, lanzan su tercer álbum, Celestial, el primer sencillo del álbum fue la canción «Ser o parecer» que fue número uno en la lista Hot Latin Songs de Billboard,​ en ese mismo año publican su primer álbum en inglés, Rebels. En 2007, lanzan Empezar desde cero, siendo nominados nuevamente en los Grammy Latinos, y finalmente en 2008, su último álbum titulado Para olvidarte de mí.
+              </p>
+              <img src="https://firebasestorage.googleapis.com/v0/b/spotifyjdab.appspot.com/o/rbd3.jpg?alt=media&token=1feeabd0-f42b-421e-9092-afc1c5572982" class="img-fluid w-100">
+              </img>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="row">
+                    <div class="col-4 text-end">
+                    <i class="bi bi-vinyl-fill fs-1"></i><br/>
+                    </div>
+                    <div class="col-8">
+                        <div>
+                            <h1>Medellin</h1>
+                            <h2>Diciembre 2</h2>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-4 text-end">
+                    <i class="bi bi-vinyl-fill fs-1"></i><br/>
+                    </div>
+                    <div class="col-8">
+                        <div>
+                            <h1>Medellin</h1>
+                            <h2>Diciembre 2</h2>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-4 text-end">
+                    <i class="bi bi-vinyl-fill fs-1"></i><br/>
+                    </div>
+                    <div class="col-8">
+                        <div>
+                            <h1>Medellin</h1>
+                            <h2>Diciembre 2</h2>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-4 text-end">
+                    <i class="bi bi-vinyl-fill fs-1"></i><br/>
+                    </div>
+                    <div class="col-8">
+                        <div>
+                            <h1>Medellin</h1>
+                            <h2>Diciembre 2</h2>
+                        </div>
+
+                    </div>
+                </div>
+            
+            
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotifyjdab.appspot.com/o/carousel1.jpg?alt=media&token=00c1b9d2-b2af-40b3-b3a4-d270888417f7" alt="" class="img-fluid w-100"></img>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotifyjdab.appspot.com/o/carousel2.jpg?alt=media&token=c00eea4f-4171-40d6-b966-d83ace3baf6a" alt="" class="img-fluid w-100"></img>
+                    </div> 
+                    <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotifyjdab.appspot.com/o/carousel3.jpg?alt=media&token=9594a49e-c367-4ccc-9981-fe2e55fd850f" alt="" class="img-fluid w-100"></img>
+                    </div>    
+                </div>
+            </div>    
+      </section>
+    </>
+  );
+}
